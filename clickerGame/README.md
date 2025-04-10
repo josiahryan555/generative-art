9e577350a61010297425c4c34f34f8db60d74be3
jryanb0b <jryanbob@gmail.com>
2025-04-10 11:58:35 +0700

    Finished basic clicker game

---
+++ clickerGame/README.md
@@ -0,0 +1,2 @@
+This is a game I made all by myself.
+Just a very basic clicker game.
--- clickerGame/sketch.js
+++ clickerGame/sketch.js
@@ -1,11 +1,19 @@
 let cash = 0;
+let winningCashAmount = 1000;
+let userWon = false;
 let horizon;
 let button;
 let clickMultiplier = 1;
+let upgradeCost = 10;
+let upgradeCostMultiplier = 2; // upgradeCost *= upgradeCostMultiplier after upgrade is clicked
 
 function setup() {
   createCanvas(800, 800);
   horizon = height / 2;
+
+  //button that upgrades the click
+  button = createButton('Upgrade Cash Per Click', 2);
+  button.mouseClicked(upgradeButtonClicked);
 }
 
 function draw() {
@@ -21,14 +29,60 @@ function draw() {
   fill("green");
   triangle(25, horizon, 45, 240, 70, horizon);
 
-  //button that upgrades the click
-  button = createButton('click multiplier',)
 
 
+  textSize(18);
   //Displays the current score
-  text(`Cash: ${cash}`, 20, 20);
+  text(`Cash: $${cash}`, 20, 20);
+  //Displays the current click multiplier
+  text(`Click multiplier: x${clickMultiplier}`, 20, 40);
+  text(`Cost of next upgrade: $${upgradeCost}`, 20, 60);
+  text(`Goal: $${winningCashAmount}`, 20, 80);
+
+  checkIfUserWon();
+  //win condition
+
+  if (userWon) {
+    textSize(80);
+    text(`You Win!`, 200, 290);
+  }
+
 }
 
 function mouseClicked() {
-  cash += 1;
+  let newCash = 1 * clickMultiplier;
+  cash += newCash;
+}
+
+// when upgrade buton is clicked, the click multiplier is increased by 1
+function upgradeButtonClicked() {
+  //check money
+  //if enough
+  if (cash >= upgradeCost) {
+    cash -= upgradeCost; // takes cash from user
+    upgradeCost *= upgradeCostMultiplier; //increases the cost of upgrades
+
+    // initiate upgrade
+    clickMultiplier += 1;
+    // change button
+
+  }
+  else {
+    // red message telling user they don't have enough
+    fill("red");
+    text("You do not have enough money for this upgrade.", 10, 200,)
+    console.log("You do not have enough money for this upgrade");
+
+  }
+  console.log("Upgrade button was clicked");
+
+  //solves problem: adding cash when user clicks the upgrade button
+  let newCash = 1 * clickMultiplier;
+  cash -= newCash;
+}
+
+function checkIfUserWon() {
+  if (cash >= winningCashAmount) {
+    userWon = true;
+  }
 }
